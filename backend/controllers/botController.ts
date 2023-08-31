5afa0b5557ecb7d3cc23db98cdfc45bb1f0c7458
MrOliveGreen <olivesondev@gmail.com>
2023-08-31 23:30:58 +0900

    feat: remove position, auto bot assign, stake nft, calc fees

--- backend/controllers/botController.ts
+++ backend/controllers/botController.ts
@@ -1,14 +1,64 @@
 import Moralis from 'moralis';
+import { createPublicClient, http, createWalletClient, formatEther } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
+import { bsc } from 'viem/chains'
+import { nonfungiblePositionManagerABI } from "~/abi/NonfungiblePositionManager";
+import { masterChefV3ABI } from "~/abi/MasterChefV3";
+import { waitUntilGas, formatCurrencyAmount } from "./functions"
+import { Token, CurrencyAmount } from '@pancakeswap/swap-sdk-core'
 const db = require("../models");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const MyPosition = db.myposition;
 
+const walletClient = createWalletClient({
+  chain: bsc,
+  transport: http(process.env.BSC_RPC_URL)
+})
+
+const publicClient = createPublicClient({
+  chain: bsc,
+  transport: http(process.env.BSC_RPC_URL)
+})
+
 const account = privateKeyToAccount(`0x${process.env.PRIVATE_KEY}`);
 
-const doAutoStake = () => {
+const doAutoStake = (tokenId: string) => {
+  return new Promise(async (resolve, reject) => {
+    const tokenOwner: any = await publicClient.readContract({
+      address: `0x${process.env.NF_V3_POSITION_MANAGER_ADDR}`,
+      abi: nonfungiblePositionManagerABI,
+      functionName: 'ownerOf',
+      args: [BigInt(tokenId)]
+    });
 
+    if (tokenOwner.toLowerCase() == account.address.toLowerCase()) { // not staked yet
+      try {
+        const wdata: any = {
+          address: `0x${process.env.NF_V3_POSITION_MANAGER_ADDR}`,
+          abi: nonfungiblePositionManagerABI,
+          functionName: 'safeTransferFrom',
+          args: [account.address, `0x${process.env.MasterChefV3_ADDR}`, BigInt(tokenId)],
+          account
+        };
+        const gasEstimated = await waitUntilGas(wdata);
+
+        if (gasEstimated) {
+          const txHash = await walletClient.writeContract(wdata);
+          if (txHash.includes(`0x`))
+            resolve(true);
+          else
+            resolve(false);
+        } else
+          resolve(false);
+      } catch (e) {
+        console.log(e);
+        resolve(false);
+      }
+    }
+    else
+      resolve(false);
+  })
 }
 
 const checkTokenIds = () => {
@@ -29,12 +79,66 @@ const checkTokenIds = () => {
           const txn = txns.find(t => t.token_address.toLowerCase() == `0x${process.env.NF_V3_POSITION_MANAGER_ADDR}` && t.transaction_hash.toLowerCase() == position.txHash.toLowerCase());
           console.log(txn);
           if (txn) {
-            await position.update({ nftId: txn.token_id });
-            doAutoStake();
+            let staked = await doAutoStake(txn.token_id);
+            await position.update({ nftId: txn.token_id, isStaked: staked });
           }
-        })
+        });
+        resolve(true);
       } else
         resolve(false);
+    } else
+      resolve(false);
+  });
+}
+
+const eth = new Token(56, `0x${process.env.BSC_PEG_ETHADDR}`, 18, 'weth', 'ETH');
+const usdc = new Token(56, `0x${process.env.BSC_PEG_USDCADDR}`, 18, 'usdc', 'USDC');
+
+const calcFees = () => {
+  return new Promise(async (resolve, reject) => {
+    const positions = await MyPosition.findAll({ where: { status: 0 } });
+    if (positions?.length > 0) {
+      for(let i = 0; i < positions.length; i ++) {
+        if(!positions[i].nftId || positions[i].isStaked == 0)
+          break;
+        
+        const tokenOwner: any = await publicClient.readContract({
+          address: `0x${process.env.NF_V3_POSITION_MANAGER_ADDR}`,
+          abi: nonfungiblePositionManagerABI,
+          functionName: 'ownerOf',
+          args: [BigInt(positions[i].nftId)]
+        });
+
+        const { result } = await publicClient.simulateContract({
+          address: `0x${process.env.NF_V3_POSITION_MANAGER_ADDR}`,
+          abi: nonfungiblePositionManagerABI,
+          functionName: 'collect',
+          args: [{ tokenId: BigInt(positions[i].nftId), recipient: tokenOwner, amount0Max: BigInt(Number.MAX_SAFE_INTEGER), amount1Max: BigInt(Number.MAX_SAFE_INTEGER) }],
+          account: tokenOwner,
+          value: BigInt(0)
+        });
+
+        const [amount0, amount1] = result;
+        const feeValue0 = CurrencyAmount.fromRawAmount(eth, amount0.toString());
+        const feeValue1 = CurrencyAmount.fromRawAmount(usdc, amount1.toString());
+        const feeEarned = JSON.stringify({ 'eth': formatCurrencyAmount(feeValue0, 4, 'en-US'), 'usdc': formatCurrencyAmount(feeValue1, 4, 'en-US') });
+
+        const updates: any = { feeEarned: feeEarned };
+        if(tokenOwner.toLowerCase() == String(`0x${process.env.MasterChefV3_ADDR}`).toLowerCase()) { // staked
+          const pendingCake = await publicClient.readContract({
+            address: `0x${process.env.MasterChefV3_ADDR}`,
+            abi: masterChefV3ABI,
+            functionName: 'pendingCake',
+            args: [BigInt(positions[i].nftId)]
+          });
+
+          updates['cakeEarned'] = formatEther(pendingCake);
+        }
+
+        await positions[i].update(updates);
+      };
+
+      resolve(true);
     } else
       resolve(false);
   });
@@ -54,4 +158,5 @@ exports.doRunBot = async () => {
   // });
 
   await checkTokenIds();
+  await calcFees();
 }
--- backend/controllers/functions.ts
+++ backend/controllers/functions.ts
@@ -1,7 +1,11 @@
 import { Token, Percent } from '@pancakeswap/sdk';
 import { Price } from '@pancakeswap/swap-sdk-core';
-import { nearestUsableTick, TickMath, TICK_SPACINGS, tickToPrice, FeeAmount, priceToClosestTick } from "@pancakeswap/v3-sdk";
-import { Pool, encodeSqrtRatioX96, NonfungiblePositionManager, Position } from '@pancakeswap/v3-sdk'
+import { nearestUsableTick, TickMath, TICK_SPACINGS, FeeAmount, priceToClosestTick } from "@pancakeswap/v3-sdk";
+import { Pool, encodeSqrtRatioX96 } from '@pancakeswap/v3-sdk';
+import { createPublicClient, createWalletClient, http, getContract } from 'viem';
+import { bsc } from 'viem/chains';
+import { PancakeV3PoolABI } from "~/abi/PancakeV3Pool";
+import { Currency, CurrencyAmount, Fraction } from '@pancakeswap/sdk'
 
 const tryParsePrice = (baseToken?: Token, quoteToken?: Token, value?: string) => {
     if (!baseToken || !quoteToken || !value) {
@@ -56,4 +60,77 @@ export const tryParseTick = (
     }
 
     return nearestUsableTick(tick, TICK_SPACINGS[feeAmount])
+}
+
+const publicClient = createPublicClient({
+    chain: bsc,
+    transport: http(process.env.BSC_RPC_URL)
+})
+
+const walletClient = createWalletClient({
+    chain: bsc,
+    transport: http(process.env.BSC_RPC_URL)
+})
+
+export const getPool = (token0: Token, token1: Token, fee: any): Promise<Pool> => {
+    return new Promise(async (resolve, reject) => {
+        const v3Pool = getContract({ abi: PancakeV3PoolABI, address: `0x${process.env.BSC_V3POOL_ADDR}`, publicClient, walletClient });
+        const slot0: any = await v3Pool.read.slot0();
+        const [sqrtPriceX96, tick, , , , feeProtocol] = slot0;
+        const liquidity: any = await v3Pool.read.liquidity();
+
+        const pool = new Pool(token0, token1, fee, sqrtPriceX96, liquidity, tick);
+        pool.feeProtocol = feeProtocol;
+
+        resolve(pool);
+    })
+}
+
+export const waitUntilGas = (wdata: any) => {
+    return new Promise(async (resolve, reject) => {
+        let tried = 0, done = false;
+
+        do {
+            try {
+                const gas = await publicClient.estimateContractGas(wdata)
+                console.log('gas:  ', gas);
+                done = true;
+                break;
+            } catch (e) {
+                tried++;
+                console.log(`Transaction failed. ${tried} time(s) tried.`);
+                await new Promise((res) => { setTimeout(res, 3000); });
+            }
+        } while (tried < 10);
+
+        resolve(done);
+    });
+}
+
+export function formatCurrencyAmount(
+    amount: CurrencyAmount<Currency> | undefined,
+    sigFigs: number,
+    locale: string,
+    fixedDecimals?: number,
+): string {
+    if (!amount) {
+        return '-'
+    }
+
+    if (amount.quotient === BigInt(0)) {
+        return '0'
+    }
+
+    if (amount.divide(amount.decimalScale).lessThan(new Fraction(1, 100000))) {
+        return `<0.0001`
+    }
+
+    const baseString = parseFloat(amount.toSignificant(sigFigs))
+    let numberString = fixedDecimals ? parseFloat(baseString.toFixed(fixedDecimals)) : baseString
+
+    return numberString.toLocaleString(locale, {
+        minimumFractionDigits: fixedDecimals,
+        maximumFractionDigits: fixedDecimals,
+        maximumSignificantDigits: fixedDecimals ? undefined : sigFigs,
+    })
 }
